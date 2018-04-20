7c50133ca05d3db59343c5aad05ee42a624fa4e3
lulu2184 <lulu2184@gmail.com>
2018-04-19 22:32:38 -0700

    util:6.6 thru:40

--- mm.c
+++ mm.c
@@ -152,7 +152,7 @@ bool mm_init(void) {
     start[1] = pack(1, true);
     heap_listp = (block_t *) &(start[1]);
 
-    block_size = 50;
+    block_size = 32;
     for (i = 0; i < NUM_LIST - 1; i++) {
         segregated_size[i] = block_size;
         block_size = (int)(block_size * 1.7 + 33);
@@ -193,7 +193,7 @@ void *malloc (size_t size) {
         return bp;
     }
 
-    asize = max(round_up(size, dsize) + dsize, min_block_size);
+    asize = round_up(size, dsize) + dsize;
     block = find_fit(asize);
     dbg_printf("Fit block: %p\n", (void*)block);
 
